9c9afac1b7a3190774b1a0214950eb23c2cf791e
martinogiacchetti <Scaffale>
2019-02-19 18:26:47 +0100

    test inline

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -2,4 +2,23 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def start!(*)
     respond_with :message, text: t('.hi')
   end
+
+  def inline_query(query, _offset)
+    query = query.first(10) # it's just an example, don't use large queries.
+    t_description = t('.description')
+    t_content = t('.content')
+    results = Array.new(5) do |i|
+      {
+        type: :article,
+        title: "#{query}-#{i}",
+        id: "#{query}-#{i}",
+        description: "#{t_description} #{i}",
+        input_message_content: {
+          message_text: "#{t_content} #{i}",
+        },
+      }
+    end
+    answer_inline_query results
+  end
+  
 end
